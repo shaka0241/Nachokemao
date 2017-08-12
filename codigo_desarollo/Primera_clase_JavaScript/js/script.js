092d11cf38ba67a620001ee0f32c9dc9fe033c20
Alberto Rojas <shaka0241@gmail.com>
2017-08-12 14:23:49 -0400

    Agregado archivos de la primera clase de JavaScript

---
+++ codigo_desarollo/Primera_clase_JavaScript/js/script.js
@@ -0,0 +1,99 @@
+// alert("hola alumnos!");//
+
+// comentarios en javaScript
+
+/* igualmente
+
+
+
+ // document.getElementById("texto").innerHTML="Hola alumnos!";
+
+document.getElementById("miBoton").onclick=function(){
+    //modificando el texto mediante una funcion
+    
+    document.getElementById("texto").innerHTML="Hola alumnos";
+} 
+
+document.getElementById("segundoBoton").onclick=function(){
+    //modificando el texto mediante una funcion
+    
+    document.getElementById("segundoparrafo").innerHTML = document.getElementById("segundoparrafo").innerHTML+"Increible";
+}
+
+
+document.getElementById("crearParrafo").onclick=function(){
+    //modificando el texto mediante una funcion
+    
+    document.getElementById("parrafoVacio").innerHTML="Bienvenidos al maravilloso mundo de JS";
+} 
+
+
+document.getElementById("botonUno").onclick=function(){
+    //modificando el texto mediante una funcion
+    
+    document.getElementById("parrafoUno").style.color="red";
+    
+    document.getElementById("parrafoUno").style.fontSize="50px";
+    
+    document.getElementById("parrafoUno").fontWeight="100";
+} 
+
+
+document.getElementById("circulo_rojo").onclick = function(){
+    document.getElementById("circulo_rojo").style.display="none";
+}
+
+document.getElementById("circulo_azul").onclick = function(){
+    document.getElementById("circulo_azul").style.display="none";
+}
+
+document.getElementById("circulo_verde").onclick = function(){
+    document.getElementById("circulo_verde").style.display="none";
+}
+
+
+
+var x=1;
+var y=4;
+var suma=x+y;
+
+console.log(x);
+console.log(y);
+console.log(suma);
+
+
+
+document.getElementById("mostrarMensaje").onclick = function(){
+    var entradatexto = "";
+    entradatexto = document.getElementById("usuario").value;
+    document.getElementById("texto").innerHTML = "Bienvenido " + entradatexto;
+}
+
+
+
+
+var miArray = new Array();
+
+    miArray[0]="rojo";
+    miArray[1]="amarillo";
+
+console.log(miArray[1]);
+
+var tweets = ["Buenos dias","Me encanta el cafe"];
+tweets.push("Vuelta al trabajo");
+tweets.splice(1,0,"Hola");
+alert(tweets[1]);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
