149e5a4a236cb7df9e3c10425f32a9bf12b5d403
Alberto Rojas <shaka0241@gmail.com>
2017-08-12 17:11:23 -0400

    Primera clase de JavaScript

---
+++ primera_clase_JavaScript/js/script.js
@@ -0,0 +1,89 @@
+// alert("Hola alumnos");
+
+/* para varias
+lineas
+
+
+
+document.getElementById("texto").innerHTML = "Hola alumnos!!";
+
+
+document.getElementById("miBoton").onclick = function(){
+    //Bloque de instrucciones
+    
+    document.getElementById("texto").innerHTML ="Hola mijos!";
+}
+
+
+
+document.getElementById("sB").onclick = function(){
+    //Bloque de instrucciones
+    
+    document.getElementById("sP").innerHTML=document.getElementById("sP").innerHTML+" Increible!!";
+}
+
+
+
+document.getElementById("cP").onclick=function(){
+    document.getElementById("pV").innerHTML="Bienvenidos al maravilloso mundo de gambal"
+}
+
+
+
+
+document.getElementById("bUno").onclick=function(){
+    
+    document.getElementById("pUno").style.color="red";
+    
+    document.getElementById("pUno").style.fontSize="80px";
+    
+    document.getElementById("pUno").style.fontWeight="100";
+}
+
+
+
+document.getElementById("circulo_rojo").onclick=function(){
+    document.getElementById("circulo_rojo").style.display="none";
+}
+
+document.getElementById("circulo_azul").onclick=function(){
+    document.getElementById("circulo_azul").style.display="none";
+}
+
+document.getElementById("circulo_verde").onclick=function(){
+    document.getElementById("circulo_verde").style.display="none";
+}
+
+
+*/
+
+
+
+
+
+var x=4;
+var y=8;
+
+var suma= x+y;
+var resta=x-y;
+var multiplicar = x*y;
+var dividir= y/x;
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicar);
+console.log(dividir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
